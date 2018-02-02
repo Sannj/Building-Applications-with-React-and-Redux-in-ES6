497b961e65579474b239ddf7f10e3022acc92e9a
Sanjana Badam <sbadam1@lmtc-sanjana.jomax.paholdings.com>
2018-02-02 15:23:37 -0700

    course till redux intro

---
+++ src/components/about/AboutPage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class AboutPage extends React.Component {
+  render(){
+    return(
+      <div>
+        <h1>About</h1>
+        <p>Random description about this course. You will learn React, React Router and Redux blah blah</p>
+      </div>
+    );
+  }
+}
+
+export default AboutPage;
---
+++ src/components/course/CoursesPage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {Link} from 'react-router';
+
+class CoursesPage extends React.Component {
+  render(){
+    return (
+      <div className="jumbotron">
+        blah
+      </div>
+    );
+  }
+}
+
+
+export default CoursesPage;
